6db159b17aca59d58096057bef39f78e1c118dd4
nurlan <nsarchalov@gmail.com>
2017-02-03 15:31:24 +0600

    login-logout

--- books/books/config/routes.rb
+++ books/books/config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+  resources	:sessions, only: [:new, :create, :destroy]
+
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :books
+
+  root 'books#index'
+
   resources :categories
   
 end
--- books/books/db/schema.rb
+++ books/books/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202182640) do
+ActiveRecord::Schema.define(version: 20170203082937) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(version: 20170202182640) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
